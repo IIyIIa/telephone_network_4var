3aaa99b0bfceb980f21cc5d23caa5b07789afdf7
Nikita Pupanov <pupanov67@mail.ru>
2024-04-30 12:16:50 +0300

    тест

---
+++ telephone_network_db/tests.py
@@ -0,0 +1,19 @@
+import unittest
+from unittest.mock import MagicMock
+from logic import *
+
+
+class TestBalanceUpdate(unittest.TestCase):
+    def test_balance_update(self):
+        existing_number = MagicMock(spec=ClientDevicesORM)
+        existing_number.Balance = 100
+        duration = 10
+        strategy = BasicHomeStrategy()
+        cost = strategy.calculate_cost(duration)
+        new_balance = existing_number.Balance - cost
+        expected_new_balance = 100 - (duration * 2)
+        self.assertEqual(new_balance, expected_new_balance)
+
+
+if __name__ == '__main__':
+    unittest.main()
