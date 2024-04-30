226e809a56eb084ba57caf5bba5306229f22fb85
Nikita Pupanov <pupanov67@mail.ru>
2024-04-30 13:42:12 +0300

    delete changes

--- telephone_network_db/delete_data.py
+++ telephone_network_db/delete_data.py
@@ -1,8 +1,8 @@
 from telephone_network_connection_to_db import connection, execute_query
 
-delete_number_call = """
-DELETE FROM Number_Call
-WHERE DeviceID = 3 and Duration = 2
+delete_client = """
+DELETE FROM Clients
+WHERE ID = 2
 """
 
-execute_query(connection, delete_number_call)
+execute_query(connection, delete_client)
